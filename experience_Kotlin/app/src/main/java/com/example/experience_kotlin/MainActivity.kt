a282f0930a64be0f1558ab6bce3c38145be05d35
Crazy_Master <gadaychenkov99@mail.ru>
2020-10-06 15:52:49 +0300

    experience

---
+++ experience_Kotlin/app/src/main/java/com/example/experience_kotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+import android.icu.text.CaseMap
+
+fun main(args: Array<String>){
+    var word: String? = null
+    var isHasCar = true
+
+    if (word == null){
+        print("переменная word = null")
+    } else {
+        print("переменная word == null")
+    }
+}
